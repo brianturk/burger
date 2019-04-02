cf74bbc16b209042216147a64d89037ca0bb9a6d
Brian Turk <bturksub@gmail.com>
2019-04-02 13:11:40 -0700

    latest

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
     $('#burger').focus();
+    var eatAudio = new Audio('../audio/Eating-SoundBible.com-1470347575.wav');
 
     $(window).resize(function () {
         if ($(window).width() < 500) {
@@ -40,7 +41,7 @@ $(document).ready(function () {
         let newB = $('<button>');
         newB.attr('data-item',element.attr('data-item'));
         newB.css('position','absolute');
-        newB.text('Devour');
+        newB.text('DEVOUR');
         newB.attr('type','button');
         newB.attr('class','btn-info devour');
         newB.attr('id','devourBtn-' + element.attr('data-item'))
@@ -56,14 +57,52 @@ $(document).ready(function () {
         if (!$(buttonId).data('clicked') && ($(buttonId).length === 0)) {
             makeDevourButton($(this));
         } else if ($(buttonId).data('clicked')) {
-            $('#eatenList').append($(buttonId).parent());
-            $(buttonId).remove();
+            eatAudio.play();
+            $($(buttonId).parent()).fadeOut('slow',function(){
+                $('#eatenList').append($(buttonId).parent());
+                $($(buttonId).parent()).fadeIn('slow',function(){
+
+                    let eatenBurger = {
+                        id: $(this).attr('data-item');
+                      };
+
+                      // Send the PUT request.
+                      $.ajax("/API/eatBurger/", {
+                        type: "PUT",
+                        data: eatenBurger
+                      }).then(
+                        function() {
+                          location.reload();
+                        }
+                      );
+                })
+            })
         }
     });
 
 
     $(document).on('click','.devour',function() {
         $(this).data('clicked',true);
+    })
+
+    $("#newBurger").on("submit", function (e) {
+        e.preventDefault();
+        //do post stuff
+
+        var newBurger = {
+            burger: $("#burger").val().trim()
+          };
+      
+          // Send the POST request.
+          $.ajax("/api/addBurger", {
+            type: "POST",
+            data: newBurger
+          }).then(
+            function() {
+              // Reload the page to get the updated list
+              location.reload();
+            }
+          );
     })
 });
 
